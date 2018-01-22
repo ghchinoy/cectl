851e05efd123daa8ef2400040ad9552857d26518
G. Hussain Chinoy <ghchinoy@gmail.com>
2018-01-22 00:40:20 -0700

    consolidates formula-instance commands

--- cmd/formula-instances.go
+++ cmd/formula-instances.go
@@ -15,7 +15,16 @@
 package cmd
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/ghchinoy/ce-go/ce"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // formula-instancesCmd represents the formula-instances command
@@ -25,11 +34,90 @@ var formulaInstancesCmd = &cobra.Command{
 	Long:  `Manage configured Instances of a Formula template`,
 }
 
+var (
+	triggerBody       string
+	triggerTextOutput bool
+)
+
+// triggerCmd represents the trigger command
+var triggerCmd = &cobra.Command{
+	Use:   "trigger <id>",
+	Short: "invoke a Formula Instance",
+	Long: `Invokes a Formula Instance by ID, resulting in a Formula Instance Execution.
+The trigger body is optional (defaults to: {}).
+This will only invoke a manually triggerable Formula.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 1 {
+			fmt.Println("must supply an ID of a Formula Instance")
+			os.Exit(1)
+		}
+
+		if !viper.IsSet(profile + ".base") {
+			fmt.Println("Can't find info for profile", profile)
+			os.Exit(1)
+		}
+
+		base := viper.Get(profile + ".base")
+		user := viper.Get(profile + ".user")
+		org := viper.Get(profile + ".org")
+
+		url := fmt.Sprintf("%s%s",
+			base,
+			fmt.Sprintf(ce.FormulaExecutionsURIFormat, args[0]),
+		)
+		auth := fmt.Sprintf("User %s, Organization %s", user, org)
+
+		client := &http.Client{}
+		req, err := http.NewRequest("POST", url, bytes.NewReader([]byte(triggerBody)))
+		if err != nil {
+			fmt.Println("Can't construct request", err.Error())
+			os.Exit(1)
+		}
+		req.Header.Add("Authorization", auth)
+		req.Header.Add("Accept", "application/json")
+		req.Header.Add("Content-Type", "application/json")
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Println("Cannot process response", err.Error())
+			os.Exit(1)
+		}
+		bodybytes, err := ioutil.ReadAll(resp.Body)
+		defer resp.Body.Close()
+
+		if outputJSON {
+			fmt.Printf("%s\n", bodybytes)
+			return
+		}
+
+		if resp.StatusCode != 200 {
+			var ex ce.FormulaInstanceCreationResponse
+			err = json.Unmarshal(bodybytes, &ex)
+			fmt.Printf("%s\nID: %v (%s)\n", ex.Message, args[0], ex.RequestID)
+			fmt.Println(resp.Status)
+			return
+		}
+
+		var ex []ce.FormulaInstanceCreationResponse
+		err = json.Unmarshal(bodybytes, &ex)
+
+		if triggerTextOutput {
+			fmt.Print(ex[0].ID)
+		} else {
+			fmt.Printf("Execution ID: %v\n", ex[0].ID)
+		}
+
+	},
+}
+
 func init() {
 	RootCmd.AddCommand(formulaInstancesCmd)
 
 	formulaInstancesCmd.PersistentFlags().StringVar(&profile, "profile", "default", "profile name")
 	formulaInstancesCmd.PersistentFlags().BoolVarP(&outputJSON, "json", "j", false, "output as json")
 	formulaInstancesCmd.PersistentFlags().BoolVarP(&showCurl, "curl", "c", false, "show curl command")
+
+	formulaInstancesCmd.AddCommand(triggerCmd)
+	triggerCmd.Flags().StringVarP(&triggerBody, "data", "d", "{}", "data for trigger body")
+	triggerCmd.Flags().BoolVarP(&triggerTextOutput, "text", "t", false, "output trigger id as text")
 
 }
